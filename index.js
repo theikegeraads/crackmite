b30ddb8b65177ef5c6437f311d8ec4cd8dbd3faa
theikegeraads <58556805+theikegeraads@users.noreply.github.com>
2020-01-09 13:48:44 +0100

    he heuj
    bot file

---
+++ index.js
@@ -0,0 +1,25 @@
+const Commando = require('discord.js-commando');
+
+const {
+    token
+} = require('./config.json');
+
+const client = new Commando.Client({
+    owner: ['465662909645848577', '464733215903580160'],
+    disableEveryone: true,
+    commandPrefix: '>',
+    unknownCommandResponse: false
+});
+
+client.registry.registerDefaultTypes()
+    .registerDefaultGroups()
+    .registerDefaultCommands({
+        help: false,
+        unknownCommand: false
+    });
+
+client.once('ready', () => {
+    console.log(`Ready! Currently in ${client.guilds.size} guilds.`);
+    client.user.setStatus('dnd');
+});
+client.login(token).then().catch(`Error logging in: ${console.error}`);
