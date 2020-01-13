73a3eb0dde9b3b8dbbd41effeee9bfd66b148f6a
Keanu <keanutimmermans8@gmail.com>
2020-01-13 18:59:22 +0000

    Added say command

--- index.js
+++ index.js
@@ -1,63 +1,26 @@
-const Discord = require('discord.js')
-const Commando = require('discord.js-commando');
-const fs = require('fs');
+const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
-const { prefix, token } = require('./config.json');
+const { token } = require('./config.json')
 
-const client = new Commando.Client({
-    owner: ['465662909645848577', '464733215903580160'],
-    disableEveryone: true,
-    commandPrefix: '>',
-    unknownCommandResponse: false
+const client = new CommandoClient({
+	commandPrefix: '>',
+	owner: ['465662909645848577', '464733215903580160']
 });
 
-client.registry.registerDefaultTypes()
-    .registerDefaultGroups()
-    .registerDefaultCommands({
-        help: false,
-        unknownCommand: false
-    });
-
-
-client.once('ready', () => {
-    console.log(`Ready! Currently in ${client.guilds.size} guilds.`);
-    client.user.setStatus('dnd');
-    client.user.setActivity('>', {
-        type: 'LISTENING'
-    });
-});
-
-client.once('reconnecting', () => {
-    console.log('Reconnecting!');
-});
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['first', 'Your First Command Group']
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
     
-client.once('disconnect', () => {
-    console.log('Disconnect!');
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity('with Commando');
 });
 
-client.on('message', async message => {
-	if (message.author.bot) return;
-});
+client.on('error', console.error);
 
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	client.commands.set(command.name, command);
-}
-
-
-client.aliases = new Discord.Collection();
-
-
-client.on('message', async message => {
-    let prefix = ".";
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const command = args.shift().toLowerCase();
-    let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
-    if (commandfile) commandfile.run(client, message, args);
-});
-
-
-client.login(token).then().catch(`Error logging in: ${console.error}`);
+client.login(token);
